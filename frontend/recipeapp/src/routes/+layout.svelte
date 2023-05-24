7188eb720d9e3c2066d28fca038b1fa04d5ba4c4
DisDRAXtion <liekens.tim@gmail.com>
2023-05-24 20:43:41 +0200

    feat(6): add navigation bar

---
+++ frontend/recipeapp/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<nav>
+	<a href="/">Home</a>
+    <a href="/about">About</a>
+    <a href="/profile">Profile</a>
+    <a href="/sign-in">Sign in</a>
+    <a href="/sign-up">Sign up</a>
+</nav>
+
+<slot />
